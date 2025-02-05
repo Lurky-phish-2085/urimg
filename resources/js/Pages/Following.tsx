505f698145ce82a9901059f9dccc0cd909d09863
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-02-05 12:57:54 +0800

    Wrap `Following` page with an`AuthenticatedLayout`

--- resources/js/Pages/Following.tsx
+++ resources/js/Pages/Following.tsx
@@ -1,3 +1,4 @@
+import Authenticated from '@/Layouts/AuthenticatedLayout';
 import { GalleryData, PageProps } from '@/types';
 
 export default function Following({
@@ -5,21 +6,29 @@ export default function Following({
 }: PageProps<{ galleries: GalleryData[] }>) {
     return (
         <>
-            {galleries.length > 0 ? (
-                galleries.map((gallery) => (
-                    <a
-                        key={gallery.id}
-                        className="my-2 block text-blue-500 underline"
-                        href={route('gallery', gallery.retrieval_id)}
-                        target="_noblank"
-                        rel="noopenner noreferrer"
-                    >
-                        {gallery.title || gallery.retrieval_id}
-                    </a>
-                ))
-            ) : (
-                <h2 className="text-lg">Nothing's here...</h2>
-            )}
+            <Authenticated
+                header={
+                    <h2 className="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+                        Following
+                    </h2>
+                }
+            >
+                {galleries.length > 0 ? (
+                    galleries.map((gallery) => (
+                        <a
+                            key={gallery.id}
+                            className="my-2 block text-blue-500 underline"
+                            href={route('gallery', gallery.retrieval_id)}
+                            target="_noblank"
+                            rel="noopenner noreferrer"
+                        >
+                            {gallery.title || gallery.retrieval_id}
+                        </a>
+                    ))
+                ) : (
+                    <h2 className="text-lg">Nothing's here...</h2>
+                )}
+            </Authenticated>
         </>
     );
 }
