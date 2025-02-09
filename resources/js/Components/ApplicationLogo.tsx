f1f0e850227c9e7e53fd92be606543028f023af7
lurkyphish2085 <95119560+Lurky-phish-2085@users.noreply.github.com>
2025-02-09 10:13:26 +0800

    Modify `ApplicationLogo`'s font size to be bigger

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -4,7 +4,7 @@ export default function ApplicationLogo({
     className = '',
 }: HTMLAttributes<HTMLDivElement> & { className?: string }) {
     return (
-        <div className={`text-xl font-black ` + className}>
+        <div className={`text-4xl font-black ` + className}>
             <span>ur</span>
             <span className="mr-1 italic">i</span>
             <span>mg</span>
